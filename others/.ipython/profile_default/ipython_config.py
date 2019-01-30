c0c7ad8d56ac53e29ece9d42e201039ac46390dd
Matthew Brett <matthew.brett@gmail.com>
2019-01-30 11:53:00 +0000

    Other setup

---
+++ others/.ipython/profile_default/ipython_config.py
@@ -0,0 +1,5 @@
+# Configuration file for all things ipython
+
+c = get_config()
+c.TerminalInteractiveShell.editing_mode = 'vi'
+c.TerminalInteractiveShell.display_completions='readlinelike'
---
+++ others/.jupyter/jupyter_notebook_config.py
@@ -0,0 +1,4 @@
+# Jupytext
+c.NotebookApp.contents_manager_class = "jupytext.TextFileContentsManager"
+c.ContentsManager.default_jupytext_formats = "ipynb,Rmd"
+c.ContentsManager.split_at_heading = True
