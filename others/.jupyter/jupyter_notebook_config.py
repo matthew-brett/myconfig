b4fb9686970af73112a3c2c06eae743a1f29a558
Matthew Brett <matthew.brett@gmail.com>
2019-02-01 22:23:42 +0000

    Allow export of metadata to Rmd etc format
    In Jupytext

--- others/.jupyter/jupyter_notebook_config.py
+++ others/.jupyter/jupyter_notebook_config.py
@@ -1,4 +1,5 @@
 # Jupytext
 c.NotebookApp.contents_manager_class = "jupytext.TextFileContentsManager"
 c.ContentsManager.default_jupytext_formats = "ipynb,Rmd"
+c.ContentsManager.default_notebook_metadata_filter = "all,-language_info"
 c.ContentsManager.split_at_heading = True
