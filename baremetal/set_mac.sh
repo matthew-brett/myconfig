d25ea559f0d29cdf153bd8c8fb449b8fe7c9da79
Matthew Brett <matthew.brett@gmail.com>
2019-02-01 22:27:31 +0000

    Updating install instructions for new Mac

--- baremetal/set_mac.sh
+++ baremetal/set_mac.sh
@@ -1,13 +1,13 @@
-# Set up a Mac Pro from bare OS install
+# Set up a Mac from bare OS install
 
 # Make some useful directories
 cd
 mkdir -p .ssh usr/bin dev_trees tmp
 
 # Set up ssh
-scp mb312@dans.dyn.berkeley.edu:.ssh/id_rsa* .ssh
+scp mb312@tom.bic.berkeley.edu:.ssh/id_rsa* .ssh
 ssh-add
-scp mb312@dans.dyn.berkeley.edu:.ssh/config .ssh
+scp mb312@tom.bic.berkeley.edu:.ssh/config .ssh
 
 # Check that xcode command line tools are installed
 xcode-select --install
@@ -20,7 +20,8 @@ xcode-select --install
 brew update
 
 # Get configuration
-brew install python3 hub
+# Python 3 is the default now
+brew install python hub
 cd dev_trees
 
 # The following needs a username and password, so run on its own.
@@ -33,21 +34,32 @@ hub clone myvim
 # More utilities
 brew install vim && brew install macvim
 
+# Bash completion
+brew install bash-completion
+
 # Set up configuration
 cat >> ~/.bash_profile << EOF
+# brew install bash-completion
+[[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && \
+    . "/usr/local/etc/profile.d/bash_completion.sh"
+
+# Default Python
+PYTHON_EXE=python3
+PIP_EXE=pip3
+
 # Source personal definitions
 if [ -f ~/.bash_personal ]; then
     . ~/.bash_personal
 fi
-
-# To source stuff for docker machine
-alias idocker="eval \$(docker-machine env)"
 EOF
 
-pip3 install virtualenvwrapper
+# To get Python, pip binaries linked
+source ~/.bash_profile
+
+#
+pip install --user virtualenvwrapper
 
 # Other setup
-brew install bash-completion
 brew cask install firefox
 brew install pandoc
 brew install pandoc-citeproc
@@ -56,7 +68,7 @@ brew cask install mactex
 brew install cabal
 brew install gnupg
 brew install pinentry-mac
-scp -r mb312@dans.dyn.berkeley.edu:.gnupg .
+scp -r mb312@tom.bic.berkeley.edu:.gnupg .
 
 # Misc
 ln -s "/System/Library/Automator/Combine PDF Pages.action/Contents/Resources/join.py" ~/usr/bin/pdfconcat
@@ -84,6 +96,18 @@ ipython profile create
 cat >> ~/.ipython/profile_default/ipython_config.py << EOF
 c.TerminalInteractiveShell.editing_mode = 'vi'
 EOF
+
+# Look in ~/dev_trees/myconfig/others for other settings
+# to copy, link, e.g
+mkdir -p .ipython .jupyter
+ln -s ~/dev_trees/myconfig/others/.jupyter/jupyter_notebook_config.sh ~/.jupyter
+ln -s ~/dev_trees/myconfig/others/.jupyter/custom ~/.jupyter
+
+# Consider
+mkdir ~/gists
+cd ~/gists
+git clone https://gist.github.com/matthew-brett/2ad2081353ad468953da4eca1d9d2112 to_gist
+ln -s ~/gists/to_gist/to_gist.sh ~/usr/bin
 
 # Printing
 # https://universityofbirmingham.service-now.com/itgateway/kb_view.do?sysparm_article=KB13216
