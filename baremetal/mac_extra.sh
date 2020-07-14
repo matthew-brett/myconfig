a78bb1644cda4a20f892a1ea4e7fd20b1fb839ad
Matthew Brett <matthew.brett@gmail.com>
2020-07-14 18:46:22 +0100

    Spacemacs configuration

--- baremetal/mac_extra.sh
+++ baremetal/mac_extra.sh
@@ -2,7 +2,10 @@
 cd ~/dev_trees
 git clone https://github.com/syl20bnr/spacemacs
 ln -s ~/dev_trees/spacemacs ~/.emacs.d
+# emacs-plus for optional dependencies of emacs
+brew tap d12frosted/emacs-plus
 brew install emacs-plus
+# source code pro as default font
 # https://github.com/syl20bnr/spacemacs/issues/3006#issuecomment-282516801
 brew tap homebrew/cask-fonts
 brew cask install font-source-code-pro --fontdir=/Library/Fonts
