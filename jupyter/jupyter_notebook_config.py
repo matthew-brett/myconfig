29f365a70ec1e88ba558a2ce9846a3702af78943
Matthew Brett <matthew.brett@gmail.com>
2022-12-03 15:17:56 +0000

    Update jupytext configuration

--- jupyter/jupyter_notebook_config.py
+++ jupyter/jupyter_notebook_config.py
@@ -1,4 +1,1 @@
-# Jupytext
-c.ContentsManager.default_jupytext_formats = "ipynb,Rmd"
-c.ContentsManager.default_notebook_metadata_filter = "all,-language_info"
-c.ContentsManager.split_at_heading = True
+# Placeholder
